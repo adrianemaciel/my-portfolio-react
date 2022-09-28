be2bb47ef735ca57649e6cbcc4513931191a4b36
adrianemaciel <adrianemacie.developer@gmail.com>
2022-09-28 18:11:00 -0300

    tech refactor

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
 import "./style.css";
-import HTML5_IMAGE from "./assets/icons8-html5.png";
-import CSS3_IMAGE from "./assets/icons8-css3.png";
-import JAVASCRIPT_IMAGE from "./assets/icons8-javascript.png";
-import GIT_IMAGE from "./assets/icons8-git.png";
-import GITHUB_IMAGE from "./assets/icons8-github.png";
-import REACT_IMAGE from "./assets/icons8-react.png";
 import WHATSAPP_IMAGE from "./assets/whatsapp-icon.png";
 import GMAIL_IMAGE from "./assets/gmail-icon.png";
 import LINKEDIN_IMAGE from "./assets/linkedin-icon.png";
 import GITHUB_LOGO_IMAGE from "./assets/github-icon.png";
 import CODER_IMAGE from "./assets/coder-workplace.png";
-import Header from "./componets/Header";
-import About from "./componets/About";
+import Header from "./components/Header";
+import About from "./components/About";
+import Tech from "./components/Tech";
 
 function App() {
   return (
     <>
       <Header />
 
-      <About/>
+      <About />
 
-      <section className="tech">
-        <div className="tech-icons">
-          <img className="icons" src={HTML5_IMAGE} alt="Logo HTML5" />
-          <img className="icons" src={CSS3_IMAGE} alt="Logo CSS3" />
-          <img className="icons" src={JAVASCRIPT_IMAGE} alt="Logo JavaScript" />
-          <img className="icons" src={GIT_IMAGE} alt="Logo Git" />
-          <img className="icons" src={GITHUB_IMAGE} alt="Logo GitHub" />
-          <img className="icons" src={REACT_IMAGE} alt="Logo React" />
-        </div>
-      </section>
+      <Tech />
 
       <section className="courses">
         <div>
