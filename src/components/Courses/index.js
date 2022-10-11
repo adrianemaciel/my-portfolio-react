9c8f4a0a9351596b8263267dac8874d0ec01b544
adrianemaciel <adrianemacie.developer@gmail.com>
2022-10-11 16:08:39 -0300

    courses refactor

--- src/components/Courses/index.js
+++ src/components/Courses/index.js
@@ -13,15 +13,17 @@ const Courses = () => {
               href="https://drive.google.com/file/d/1rhL5yqF4wPmmt04XKFnXuf5wzIoj6bJL/view?usp=sharing"
             >
               <img
-                src="https://media-exp2.licdn.com/dms/image/C4E0BAQFRgsykuHzwZw/company-logo_200_200/0/1642449006436?e=1664409600&v=beta&t=8FU6W3LXrxIR_ZfkBazcD2ANk4kma3DdNAI9uJ3hkuQ"
+                className="cards-image"
+                src="https://media-exp1.licdn.com/dms/image/C4D0BAQGYE7b1n_6bKA/company-logo_200_200/0/1657647421523?e=1673481600&v=beta&t=-DHnnsTS82FJXmH7888TN1E-5egOlhE_2FhmHPv8JVo"
                 alt="Logo IOS - Instituto da Oportunidade Social"
               />
             </a>
-            <h3>Introdução a Programação Web</h3>
+            <h3>Introdução à Programação Web</h3>
             <p>
-              What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
-              printing and typesetting industry. Lorem Ipsum has been the
-              industry's standard dummy text
+              Este curso abordou conceitos do funcionamento das
+              telecomunicações, protocolos e fluxos de dados da internet;
+              Conceitos e práticas com as linguagens HTML5 e CSS3; Comunicação e
+              Expressão; Cálculo Matemático;
             </p>
           </div>
           <div className="cards">
@@ -30,15 +32,16 @@ const Courses = () => {
               href="https://drive.google.com/file/d/10ezmecFDMLjaWsjcxrOepmdizjLQpcmb/view?usp=sharing"
             >
               <img
-                src="https://media-exp2.licdn.com/dms/image/C4E0BAQFOiaQvbDY3hQ/company-logo_200_200/0/1547486273811?e=1664409600&v=beta&t=f7MAEMai7qin41CWNLXxtYbIiSX5a3EE2T4idiN-pbs"
-                alt="Logo Fundação Bradesco - Escola Virtual"
+                className="cards-image"
+                src="https://media-exp1.licdn.com/dms/image/C4E0BAQFOiaQvbDY3hQ/company-logo_200_200/0/1547486273811?e=1673481600&v=beta&t=CVgHfG1MLLHTv5fmWly5_daNOtwuKXB9uSFP2nD8OWk"
+                alt="Logo da organização Fundação Bradesco"
               />
             </a>
             <h3>HTML Básico</h3>
             <p>
-              What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
-              printing and typesetting industry. Lorem Ipsum has been the
-              industry's standard dummy text
+              Este curso foi desenvolvido para auxiliar na criação de páginas
+              Web, por meio do Hyper Text Markup Language – mais conhecido como
+              HTML.
             </p>
           </div>
           <div className="cards">
@@ -47,15 +50,17 @@ const Courses = () => {
               href="https://drive.google.com/file/d/1aW6_xSYeaDjAQ7wGf9fD_Nv992Sq35MZ/view?usp=sharing"
             >
               <img
-                src="https://media-exp2.licdn.com/dms/image/C4E0BAQFOiaQvbDY3hQ/company-logo_200_200/0/1547486273811?e=1664409600&v=beta&t=f7MAEMai7qin41CWNLXxtYbIiSX5a3EE2T4idiN-pbs"
-                alt="Logo Fundação Bradesco - Escola Virtual"
+                className="cards-image"
+                src="https://media-exp1.licdn.com/dms/image/C4E0BAQFOiaQvbDY3hQ/company-logo_200_200/0/1547486273811?e=1673481600&v=beta&t=CVgHfG1MLLHTv5fmWly5_daNOtwuKXB9uSFP2nD8OWk"
+                alt="Logo da organização Fundação Bradesco"
               />
             </a>
             <h3>HTML Avançado</h3>
             <p>
-              What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
-              printing and typesetting industry. Lorem Ipsum has been the
-              industry's standard dummy text
+              Este curso abordou conceitos que complementam o conteúdo
+              apresentado no curso de HTML – Básico. Agora, aprendendo a criar e
+              formatar tabelas, frames e formulários, além de documentar
+              páginas.
             </p>
           </div>
           <div className="cards">
@@ -64,15 +69,16 @@ const Courses = () => {
               href="https://drive.google.com/file/d/1nKAWEl8Eyp4Hz7KQFEn2lQhnK2kWGs3r/view?usp=sharing"
             >
               <img
-                src="https://media-exp2.licdn.com/dms/image/C4D0BAQHWY4ymyl70yg/company-logo_200_200/0/1654536512339?e=1664409600&v=beta&t=vsOuw_kDA1zBZqgpidw2MUoCNgPrf0AUzmXJUmGUk-0"
-                alt=""
+                className="cards-image"
+                src="https://media-exp1.licdn.com/dms/image/C4D0BAQHU-m2Q6FnwDw/company-logo_200_200/0/1657132506446?e=1673481600&v=beta&t=lv3JIOD-HUsvbmBrOYuwJv-yUbcyQDe0gme9ki2c7kM"
+                alt="Logo da organização Digital Innovation One Inc."
               />
             </a>
             <h3>Introdução a criação de websites com HTML5 E CSS3</h3>
             <p>
-              What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
-              printing and typesetting industry. Lorem Ipsum has been the
-              industry's standard dummy text
+              Este curso abordou a história e o funcionamento do HTML5 e do
+              CSS3, a estrutura básica dessas tecnologias, sua semântica,
+              principais elementos e comandos.
             </p>
           </div>
           <div className="cards">
@@ -81,15 +87,17 @@ const Courses = () => {
               href="https://drive.google.com/file/d/1PBybBWvhMZgWj3v8PioLHjHrLh5UZztz/view?usp=sharing"
             >
               <img
-                src="https://media-exp2.licdn.com/dms/image/C4D0BAQHWY4ymyl70yg/company-logo_200_200/0/1654536512339?e=1664409600&v=beta&t=vsOuw_kDA1zBZqgpidw2MUoCNgPrf0AUzmXJUmGUk-0"
-                alt=""
+                className="cards-image"
+                src="https://media-exp1.licdn.com/dms/image/C4D0BAQHU-m2Q6FnwDw/company-logo_200_200/0/1657132506446?e=1673481600&v=beta&t=lv3JIOD-HUsvbmBrOYuwJv-yUbcyQDe0gme9ki2c7kM"
+                alt="Logo da organização Digital Innovation One Inc."
               />
             </a>
             <h3>Introdução ao Git e ao GitHub</h3>
             <p>
-              What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
-              printing and typesetting industry. Lorem Ipsum has been the
-              industry's standard dummy text
+              Este curso abordou o que é controle de versão e como trabalhar com
+              o Git, assim como os seus principais comandos. Introdução ao
+              GitHub e sua interface WEB com exemplos de uso de branch e merge
+              com um fluxo para trabalhar em equipe e usar no mundo real.
             </p>
           </div>
           <div className="cards">
@@ -98,15 +106,51 @@ const Courses = () => {
               href="https://drive.google.com/file/d/19BSbpmegGnvpAyYMklnGOe5LTU9Gaz7n/view?usp=sharing"
             >
               <img
-                src="https://media-exp2.licdn.com/dms/image/C4D0BAQHWY4ymyl70yg/company-logo_200_200/0/1654536512339?e=1664409600&v=beta&t=vsOuw_kDA1zBZqgpidw2MUoCNgPrf0AUzmXJUmGUk-0"
-                alt=""
+                className="cards-image"
+                src="https://media-exp1.licdn.com/dms/image/C4D0BAQHU-m2Q6FnwDw/company-logo_200_200/0/1657132506446?e=1673481600&v=beta&t=lv3JIOD-HUsvbmBrOYuwJv-yUbcyQDe0gme9ki2c7kM"
+                alt="Logo da organização Digital Innovation One Inc."
               />
             </a>
             <h3>Lógica de Programação Essencial</h3>
             <p>
-              What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
-              printing and typesetting industry. Lorem Ipsum has been the
-              industry's standard dummy text.
+              Este curso abordou o desenvolvimento de lógica de programação, o
+              uso de algoritmos que são sequências de passos bem estabelecidos,
+              como por exemplo, uma receita de bolo.
+            </p>
+          </div>
+          <div className="cards">
+            <a
+              target="_blank"
+              href="https://drive.google.com/file/d/19BSbpmegGnvpAyYMklnGOe5LTU9Gaz7n/view?usp=sharing"
+            >
+              <img
+                className="cards-image"
+                src="https://media-exp1.licdn.com/dms/image/C4D0BAQHU-m2Q6FnwDw/company-logo_200_200/0/1657132506446?e=1673481600&v=beta&t=lv3JIOD-HUsvbmBrOYuwJv-yUbcyQDe0gme9ki2c7kM"
+                alt="Logo da organização Digital Innovation One Inc."
+              />
+            </a>
+            <h3>Trabalhando com Componentes em React</h3>
+            <p>
+              Este curso abordou sobre a estrutura e ciclo de vida dos
+              componentes React. Explore conceitos de Single Page Application
+              (SPA), modularização e boas práticas com componentes React.
+            </p>
+          </div>
+          <div className="cards">
+            <a
+              target="_blank"
+              href="https://drive.google.com/file/d/19BSbpmegGnvpAyYMklnGOe5LTU9Gaz7n/view?usp=sharing"
+            >
+              <img
+                className="cards-image"
+                src="https://media-exp1.licdn.com/dms/image/C4D0BAQHU-m2Q6FnwDw/company-logo_200_200/0/1657132506446?e=1673481600&v=beta&t=lv3JIOD-HUsvbmBrOYuwJv-yUbcyQDe0gme9ki2c7kM"
+                alt="Logo da organização Digital Innovation One Inc."
+              />
+            </a>
+            <h3>Sintaxe Básica em JavaScript</h3>
+            <p>
+              O curso tem como objetivo geral explicar de forma didática o que é
+              o JavaScript, para que serve e como utilizá-lo em projetos.
             </p>
           </div>
         </div>
