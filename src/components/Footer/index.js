b6fdd6c362e51dd0d3f5dc162ab76f8b8b9b2a96
adrianemaciel <adrianemaciel.developer@gmail.com>
2022-10-24 17:31:29 -0300

    footer code refactor

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,19 +11,30 @@ const Footer = () => {
     <footer className="footer">
       <div className="social-links">
         <div className="footer-image">
-          <img className="footer-icon" src={CODER_IMAGE} alt="local de trabalho do codificador" />
+          <img
+            className="footer-icon"
+            src={CODER_IMAGE}
+            alt="local de trabalho do codificador"
+          />
         </div>
         <div className="col">
-          <h2>Contato</h2>
           <div className="icon-link">
             <a href="http://api.whatsapp.com/send?1=pt_BR&phone=5511946353112">
-              <img className="footer-icon" src={WHATSAPP_IMAGE} alt="Icone Whatsapp" />
+              <img
+                className="footer-icon"
+                src={WHATSAPP_IMAGE}
+                alt="Icone Whatsapp"
+              />
               <span>(11)94635-3112</span>
             </a>
           </div>
           <div className="icon-link">
             <a href="mailto:adrianemaciel.developer@gmail.com">
-              <img className="footer-icon" src={GMAIL_IMAGE} alt="Icone Gmail" />
+              <img
+                className="footer-icon"
+                src={GMAIL_IMAGE}
+                alt="Icone Gmail"
+              />
               <span>adrianemaciel.developer@gmail.com</span>
             </a>
           </div>
@@ -33,15 +44,23 @@ const Footer = () => {
           <h2 className="footer-title">Desenvolvedora</h2>
           <div>
             <p className="footer-text">Densenvolvido por Adriane Maciel</p>
-            <p className="footer-text">Todos os Direitos Reservados</p> 
+            <p className="footer-text">Todos os Direitos Reservados</p>
             <a
               href="https://www.linkedin.com/in/adrianemacieldeveloper/"
               target="_blank"
             >
-              <img className="footer-icon" src={LINKEDIN_IMAGE} alt="Icone Linkedin" />
+              <img
+                className="footer-icon"
+                src={LINKEDIN_IMAGE}
+                alt="Icone Linkedin"
+              />
             </a>
             <a href="https://github.com/adrianemaciel" target="_blank">
-              <img className="footer-icon" src={GITHUB_LOGO_IMAGE} alt="Icone GitHub" />
+              <img
+                className="footer-icon"
+                src={GITHUB_LOGO_IMAGE}
+                alt="Icone GitHub"
+              />
             </a>
           </div>
         </div>
