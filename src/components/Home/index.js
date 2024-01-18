d0fe546ce7d93a1810f47c1516adcc13a0b5d16c
adrianemaciel <adrianemaciel.developer@gmail.com>
2024-01-18 16:02:16 -0300

    refactor: section home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,23 +1,17 @@
 import React from "react";
 import "./styles.css";
 import CODE_BANNER_IMAGE from "../../assets/code-banner.png";
-import CURRICULO_PDF from "../../assets/curriculo-adriane-maciel.pdf";
 
 const Home = () => {
   return (
     <section className="wrapper" id="#scroll-about">
       <div className="about">
         <div className="wrapper-text">
-          <span>Desenvolvedora Frontend</span>
           <h1>Adriane Maciel</h1>
-          <p>
-            Seja bem-vindo(a) a minha página pessoal. Aqui colocarei alguns
-            projetos desenvolvidos e as tecnologias usadas durante o processo.
-          </p>
-
-          <a href={CURRICULO_PDF} download="Currículo Adriane Maciel">
-            <button>Baixar currículo</button>
-          </a>
+          <span>
+            Desenvolvedora Frontend e <br/> Graduanda em Análise e Desenvolvimento de
+            Sistemas
+          </span>
         </div>
         <div className="wrapper-image">
           <img src={CODE_BANNER_IMAGE} alt="Banner" />
