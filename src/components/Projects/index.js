df80ccbbf5f64f5fd8e34220c1d57c255fcddcd9
adrianemaciel <adrianemaciel.developer@gmail.com>
2024-03-07 15:17:01 -0300

    refactor: simplifying mapping

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,90 +1,58 @@
 import React, { useEffect, useState } from "react";
 import "./styles.css";
 
+const PROJECT_NAMES = [
+  "teste-desafio-m3",
+  "fork-frontend-vagas",
+  "my-portfolio-react",
+  "nunes-sports",
+  "poke-app",
+];
+
 const Projects = () => {
-  const [repoList, setRepoList] = useState();
+  const [repoList, setRepoList] = useState([]);
 
   useEffect(() => {
-    fetch("https://api.github.com/users/adrianemaciel/repos")
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          const repoItem = result.map((repo) => {
-            return {
-              name: repo.name,
-              description: repo.description,
-              html_url: repo.html_url,
-            };
-          });
+    const fetchRepoData = async () => {
+      try {
+        const response = await fetch("https://api.github.com/users/adrianemaciel/repos");
+        const result = await response.json();
 
-          const poke_app = repoItem.filter(
-            (repo) => repo.name === "poke-app"
-          )[0];
+        const filteredRepos = result
+          .filter((repo) => PROJECT_NAMES.includes(repo.name))
+          .map((repo) => ({
+            name: repo.name,
+            description: repo.description,
+            html_url: repo.html_url,
+          }));
 
-          const desafio_nunes_sports = repoItem.filter(
-            (repo) => repo.name === "nunes-sports"
-          )[0];
+        setRepoList(filteredRepos);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-          const portfolio = repoItem.filter(
-            (repo) => repo.name === "my-portfolio-react"
-          )[0];
-
-          const testeFmd = repoItem.filter(
-            (repo) => repo.name === "fork-frontend-vagas"
-          )[0];
-
-          const testeM3 = repoItem.filter(
-            (repo) => repo.name === "teste-desafio-m3"
-          )[0];
-
-          const nlw = repoItem.filter(
-            (repo) => repo.name === "nlw-return-rocketseat"
-          )[0];
-
-          const yoga = repoItem.filter(
-            (repo) => repo.name === "projetoyoga"
-          )[0];
-
-          const lgbt = repoItem.filter((repo) => repo.name === "LGBTQIA-")[0];
-
-          setRepoList([
-            nlw,
-            yoga,
-            lgbt,
-            testeM3,
-            testeFmd,
-            portfolio,
-            desafio_nunes_sports,
-            poke_app,
-          ]);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+    fetchRepoData();
   }, []);
 
   return (
     <section className="section-projects" id="projects">
       <h2>Projetos</h2>
       <div className="wrapper-projects">
-        {repoList &&
-          repoList.map((repo, index) => {
-            return (
-              <div className="project-cards" key={index}>
-                <div className="project">
-                  <a target="_blank" rel="noreferrer" href={repo.html_url}>
-                    <img
-                      src="https://i.postimg.cc/26HV8Q4L/octocat-1664567542809.png"
-                      alt="Octocat"
-                    />
-                  </a>
-                  <h3>{repo.name}</h3>
-                  <p>{repo.description}</p>
-                </div>
-              </div>
-            );
-          })}
+        {repoList.map((repo, index) => (
+          <div className="project-cards" key={index}>
+            <div className="project">
+              <a target="_blank" rel="noreferrer" href={repo.html_url}>
+                <img
+                  src="https://i.postimg.cc/26HV8Q4L/octocat-1664567542809.png"
+                  alt="Octocat"
+                />
+              </a>
+              <h3>{repo.name}</h3>
+              <p>{repo.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
